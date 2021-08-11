991c3ca4fe03525537c0dcd6071c947776895d0b
Michael <jjk9469@gmail.com>
2021-08-11 02:32:45 -0700

    edit controllers/campgrounds.js (updateCapground)

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -55,20 +55,31 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    console.log(req.body);
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    const geoData = await geocoder
+      .forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1,
+      })
+      .send();
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
+    campground.geometry = geoData.body.features[0].geometry;
     await campground.save();
     if (req.body.deleteImages) {
-        for (let filename of req.body.deleteImages) {
-            await cloudinary.uploader.destroy(filename);
-        }
-        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+      for (let filename of req.body.deleteImages) {
+        await cloudinary.uploader.destroy(filename);
+      }
+      await campground.updateOne({
+        $pull: { images: { filename: { $in: req.body.deleteImages } } },
+      });
     }
-    req.flash('success', 'Successfully updated campground!');
-    res.redirect(`/campgrounds/${campground._id}`)
-}
+    console.log(campground);
+    req.flash("success", "Successfully update a campground");
+    res.redirect(`/campgrounds/${campground._id}`);
+};
 
 module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
